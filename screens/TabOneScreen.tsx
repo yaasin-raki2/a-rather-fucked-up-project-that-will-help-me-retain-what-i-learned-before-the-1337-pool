204d02c70efd9bad501b842895f43b56607bb8aa
yaasin-raki2 <yassin23raki@gmail.com>
2021-09-27 12:45:41 +0100

    created a chat component where we have the image aligned next to the name the last message and the time it was sent in

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,33 +1,56 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { Text, Image, View, StyleSheet } from "react-native";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+export default function TabOneScreen() {
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{
+                    uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+                }}
+                style={styles.image}
+            />
+            <View style={styles.rightContainer}>
+                <View style={styles.row}>
+                    <Text style={styles.name}>Elon Musk</Text>
+                    <Text style={styles.text}>11:11 AM</Text>
+                </View>
+                <View>
+                    <Text numberOfLines={1} style={styles.text}>
+                        uityzdfoqslic ki yfvol jef eou ylyhvuoyhrfdbrf ou
+                    </Text>
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    container: {
+        flexDirection: "row",
+        padding: 10,
+    },
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 30,
+        marginRight: 10,
+    },
+    rightContainer: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    name: {
+        fontWeight: "bold",
+        fontSize: 18,
+        marginBottom: 3,
+    },
+    text: {
+        color: "grey",
+    },
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
 });
