79dd448363ad56b18443767b6bb626d603cd5600
yaasin-raki2 <yassin23raki@gmail.com>
2021-09-28 16:55:17 +0100

    added some logic and functionality to the message input component (still there is a lot more to add git add .!

--- components/MessageInput/MessageInput.tsx
+++ components/MessageInput/MessageInput.tsx
@@ -1,15 +1,44 @@
-import React from "react";
-import { View, TextInput } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Pressable, KeyboardAvoidingView, Platform } from "react-native";
 import { AntDesign, Feather, Ionicons } from "@expo/vector-icons";
 
 import { styles } from "./styles";
 
 const MessageInput = () => {
+    const [message, setMessage] = useState<string>("");
+
+    const sendMessage = () => {
+        //Handle send message
+        console.warn("sending: ", message);
+    };
+
+    const onPlusPressed = () => {
+        //Handle plus press
+        console.warn("plus pressed");
+    };
+
+    const onPress = () => {
+        if (message) {
+            sendMessage();
+        } else {
+            onPlusPressed();
+        }
+    };
+
     return (
-        <View style={styles.root}>
+        <KeyboardAvoidingView
+            style={styles.root}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={100}
+        >
             <View style={styles.inputContainer}>
                 <AntDesign name="smileo" size={24} color="#595959" style={styles.icon} />
-                <TextInput placeholder="Signal message" style={styles.input} />
+                <TextInput
+                    placeholder="Signal message"
+                    style={styles.input}
+                    value={message}
+                    onChangeText={setMessage}
+                />
                 <Feather name="camera" size={24} color="#595959" style={styles.icon} />
                 <Ionicons
                     name="mic-outline"
@@ -18,10 +47,14 @@ const MessageInput = () => {
                     style={styles.icon}
                 />
             </View>
-            <View style={styles.buttonContainer}>
-                <AntDesign name="plus" size={24} color="white" />
-            </View>
-        </View>
+            <Pressable onPress={onPress} style={styles.buttonContainer}>
+                {message ? (
+                    <Ionicons name="send" size={18} color="white" />
+                ) : (
+                    <AntDesign name="plus" size={24} color="white" />
+                )}
+            </Pressable>
+        </KeyboardAvoidingView>
     );
 };
 
